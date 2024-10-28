968fa4641c436e69f9ac2de42cbf66f93f26ab6b
DmytroHalai <halai.dmytro@lll.kpi.ua>
2024-10-28 13:42:37 +0200

    Add restfull service realisation

--- src/scripts/src/main/java/com/example/db/model/User.java
+++ src/scripts/src/main/java/com/example/db/model/User.java
@@ -16,6 +16,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
     @Column(name = "first_name", nullable = false)
     private String first_name;
 
